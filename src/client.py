3f71856843243028990b3c0426a5211d560da4fd
haru <kawachigenji95@gmail.com>
2023-09-19 00:06:45 +0900

    Add function to just create a chatroom, can't join from there.

--- src/client.py
+++ src/client.py
@@ -1,20 +1,63 @@
 import socket
 import sys
+import os
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = "127.0.0.1"
-server_port = 9001
+
+def getResponse(socket) -> str:
+    response: bytes = sock.recv(BUFFER_SIZE)
+    return response.decode("utf-8")
+
+
+def login(socket) -> None:
+    username: str = input("username: ")
+    sock.send(username.encode("utf-8"))
+
+
+sock: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address: str = "127.0.0.1"
+server_port: int = 9001
+BUFFER_SIZE: int = 256
 
 print("connecting to {}".format((server_address, server_port)))
 
-try:
-    sock.connect((server_address, server_port))
-    sock.send(b"sample room:4:hogehoge")
-    sock.send(b"sample room:join")
-    sock.send(b"sample room:create:3")
-except socket.error as err:
-    print(err)
-    sys.exit(1)
-finally:
-    print("closing socket")
-    sock.close()
+while True:
+    try:
+        sock.connect((server_address, server_port))
+        login(sock)
+        while True:
+            command: str = input(">> ")
+            if command == "create":
+                title: str = input("Title of chatroom > ")
+                max_num_of_participants: int = int(
+                    input("Max number of participants > ")
+                )
+                request_data: str = f"create:{title}:{max_num_of_participants}\n"
+                sock.send(request_data.encode("utf-8"))
+            elif command == "join":
+                pass
+            elif command == "list":
+                sock.send("list\n".encode("utf-8"))
+            elif command == "close":
+                sock.send("close\n".encode("utf-8"))
+                break
+            elif command == "help":
+                current_file_path: str = os.path.abspath(__file__)
+                current_directory = os.path.dirname(current_file_path)
+                manual_file_path = os.path.join(
+                    current_directory, "../man", "manual.txt"
+                )
+                with open(manual_file_path, "r") as f:
+                    manual: str = f.read()
+                    print("\n" + manual + "\n")
+            else:
+                print("Invalid command. Type 'help' to find out how to operate.")
+                continue
+            response: str = getResponse(sock)
+            print(response)
+    except Exception as err:
+        print(err)
+        sys.exit(1)
+    finally:
+        print("Closing socket")
+        sock.close()
+        sys.exit(0)
--- src/server.py
+++ src/server.py
@@ -3,16 +3,17 @@ import chatroom
 
 room_title_to_chatrooms: dict[str, chatroom.ChatRoom] = {}
 client_key_to_chatrooms: dict[str, chatroom.ChatRoom] = {}
-MAX_NUM_OF_PARTICIPANTS: int = 3
 BUFFER_SIZE: int = 256
-TIMEOUT_SECONDS: float = 10.0
+TIMEOUT_SECONDS: float = 60.0
 
 
-def createChatRoom(room_title: str) -> None:
+def createChatRoom(
+    room_title: str, max_num_of_participants: int, host: chatroom.ChatClient
+) -> None:
     if room_title in room_title_to_chatrooms:
         raise Exception(f"Chatroom '{room_title}' already exists")
     new_chatroom = chatroom.ChatRoom(
-        title=room_title, max_num_of_participants=MAX_NUM_OF_PARTICIPANTS
+        title=room_title, max_num_of_participants=max_num_of_participants, host=host
     )
     room_title_to_chatrooms[room_title] = new_chatroom
     print(f"Chatroom '{room_title}' was created")
@@ -35,7 +36,6 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = "127.0.0.1"
     server_port = 9001
-
     print("Starting up on {} port {}".format(server_address, server_port))
 
     sock.bind((server_address, server_port))
@@ -43,18 +43,43 @@ def main():
 
     while True:
         client_socket, client_address = sock.accept()
+        print("connection from", client_address)
         client_socket.settimeout(TIMEOUT_SECONDS)
         try:
-            print("connection from ", client_address)
-            data: bytes = client_socket.recv(BUFFER_SIZE)
-            print("received data -> ", data)
-            whole_data_string: str = ""
-            while data is not None and data != b"":
-                whole_data_string += data.decode("utf-8")
-                data = client_socket.recv(BUFFER_SIZE)
-                print("received data -> ", data)
-            print("received data -> ", whole_data_string)
-            print("receive all data from ", client_address)
+            username = client_socket.recv(BUFFER_SIZE)
+            client = chatroom.ChatClient(client_address[0], client_address[1], username)
+            data_buffer: bytes = b""
+            line: bytes = b""
+            isActiveConnection = True
+            while isActiveConnection:
+                while True:
+                    chunk = client_socket.recv(BUFFER_SIZE)
+                    print("received data ->", chunk)
+                    if chunk == b"":
+                        isActiveConnection = False
+                        break
+                    data_buffer += chunk
+                    if b"\n" in data_buffer:
+                        line, data_buffer = data_buffer.split(b"\n", 1)
+                        break
+                words: list[str] = line.decode("utf-8").split(":")
+                print(f"line -> {line}")
+                print(f"data_buffer -> {data_buffer}")
+                print(f"words -> {words}")
+                if words[0] == "list":
+                    titles: list[str] = list(room_title_to_chatrooms.keys())
+                    client_socket.send(",".join(titles).encode("utf-8"))
+                elif words[0] == "create":
+                    title: str = words[1]
+                    max_num_of_participants: str = words[2]
+                    createChatRoom(title, int(max_num_of_participants), client)
+                    client_socket.send(
+                        f"New chatroom '{title}' was created".encode("utf-8")
+                    )
+                elif words[0] == "close":
+                    break
+                else:
+                    pass
         except socket.timeout:
             print(
                 f"No data received in {TIMEOUT_SECONDS} seconds. "
@@ -62,8 +87,9 @@ def main():
             )
         except Exception as e:
             print("Error: " + str(e))
+            client_socket.send(str(e).encode("utf-8"))
         finally:
-            print("Closing current connection to ", client_address)
+            print("Closing current connection to", client_address)
             client_socket.close()
 
 
